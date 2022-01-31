79addf3b85356b6dbd3386c8246ec1bf004c2ff5
MahmoudArobie <41952155+M98abas@users.noreply.github.com>
2022-01-31 22:59:41 +0300

    adding

--- prisma/index.ts
+++ prisma/index.ts
@@ -1,9 +1,33 @@
+import notFound from "../middleware/notFound";
+import cors from "cors";
+import * as express from "express";
+import productsRoute from "../routes/activties/products";
+import categoriesRoute from "../routes/activties/categories";
+import adminRoute from "../routes/web/admin";
+import customersRoute from "../routes/web/customers";
+import ordersRoute from "../routes/activties/orders";
 import { PrismaClient } from ".prisma/client";
-import app from "./app";
+
 const prisma = new PrismaClient();
 
 async function main() {
-  app.listen(3004, () =>
+  const app = express();
+  app.use(cors());
+  app.use(express.json());
+  // product route
+  app.use("/product", productsRoute);
+  // orders
+  app.use("/orders", ordersRoute);
+  // category
+  app.use("/category", categoriesRoute);
+  // admin
+  app.use("/admin", adminRoute);
+  // customers
+  app.use("/customer", customersRoute);
+  // not found route
+  app.use(notFound);
+
+  app.listen(3000, () =>
     console.log("REST API server ready at: http://localhost:3000/v1")
   );
 }
