4433aabac15ab47ba3d5f7186e809619c5f126c8
MahmoudArobie <41952155+M98abas@users.noreply.github.com>
2022-01-31 22:30:35 +0300

    try

--- prisma/app.ts
+++ prisma/app.ts
@@ -9,13 +9,15 @@ import ordersRoute from "../routes/activties/orders";
 
 const route = express.Router();
 const app = express();
+
+app.use(express.json());
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, 
      Content-Type, Accept");
     next();
 });
-app.use(express.json());
 // product route
 route.use("/product", productsRoute);
 // orders
